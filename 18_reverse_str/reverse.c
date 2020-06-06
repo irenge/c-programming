220a746a179c4332389de010dd3be74eff708910
Coursera Learner <nobody@nowhere.nul>
2020-06-06 16:06:03 +0000

    trial

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -3,19 +3,74 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //WRITE ME!
- /* int i,j;
-  int len = strlen(str) - 1;
-  char less[len+1];
-  
-  for (i = len, j = 0;j<= len ; i--,j++ ) 
-    less[j] = str[i]; 
-  for( i = 0 ; i <=len ; i++ )
-    str[i] = less[i];
+  if (str)
+    {
+      char * end = str + strlen(str) - 1;
+
+      // swap the values in the two given variables
+      // XXX: fails when a and b refer to same memory location
+#   define XOR_SWAP(a,b) do\
+	{\
+	  a ^= b;\
+	  b ^= a;\
+	  a ^= b;\
+	} while (0)
+
+      // walk inwards from both ends of the string,
+      // swapping until we get to the middle
+      while (str < end)
+	{
+	  XOR_SWAP(*str, *end);
+	  str++;
+	  end--;
+	}
+      #   undef XOR_SWAP
+    }
+}
+
+
+
+
+
+  /*char temp;
+  size_t len = strlen(str) - 1;
+  size_t i;
+  size_t k = len;
+
+  for(i = 0; i < len; i++)
+    {
+      temp = str[k];
+      str[k] = str[i];
+      str[i] = temp;
+      k--;
+  */
+      /*As 2 characters are changing place for each cycle of the loop
+	 only traverse half the array of characters */
+  /*    if(k == (len / 2))
+	{
+	  break;
+	}
+    }
+}
 
 
 */
+/*//WRITE ME!
+  int i,j;
+  int len = strlen(str) - 1;
+  char less[len];
+  
+  for (i = len, j = 0;j<= len ;  ) 
+    less[j++] = str[i--];
+  less[len] = '\0';
+  for( i = 0 ; i <=len ; i++ )
+    str[i] = less[i];
+  str[i+1] = '\0';
+}
 
+*/
+
+  /*
  int j, i;
     int tmpry = 0;
 
@@ -28,8 +83,15 @@ void reverse(char * str) {
     }
  
 }
-    
-  
+  */
+/*
+  char *p1 = str;
+  char *p2 = str;
+
+  while (*p1 != '\0')
+    p
+}
+*/  
 
 int main(void) {
   char str0[] = "";
