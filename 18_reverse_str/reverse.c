7913fecee22ecf651a1e0e1f0297520fa7d87e8f
Coursera Learner <nobody@nowhere.nul>
2020-06-07 22:33:17 +0000

    18

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -34,8 +34,8 @@ int main(void) {
   char str0[] = "";
   char str1[] = "123";
   char str2[] = "abcd";
-  char str3[] = "Captain's loj, Stardate 42523.7";
-  char str4[] = "Hello, my name is Inijo Montoya.";
+  char str3[] = "Captain's log, Stardate 42523.7";
+  char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
   char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
