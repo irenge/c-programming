d9936ef09edea5b1a3cfccd7ec429ff97d047f9c
Coursera Learner <nobody@nowhere.nul>
2020-06-08 19:16:49 +0000

    20

---
+++ 20_rot_matrix/rotate.c
@@ -0,0 +1,18 @@
+void rotate(char matrix[10][10]) {
+int i,j;
+char temp;
+
+for ( i = 0; i < 10 ; i++)   {
+    
+    for ( j = 0; j < 5; j++) {
+      
+      temp =matrix[i][j];
+      
+      matrix[i][j]=matrix[i][9-j];
+      
+      matrix[i][9-j]=temp;
+      
+    }
+    
+  }
+}
