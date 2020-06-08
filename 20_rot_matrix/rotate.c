d9038831c1c1060248da9107361b959f92938a92
Coursera Learner <nobody@nowhere.nul>
2020-06-08 20:06:14 +0000

    20

--- 20_rot_matrix/rotate.c
+++ 20_rot_matrix/rotate.c
@@ -1,4 +1,4 @@
-void rotate(char matrix[10][10]) {
+/*void rotate(char matrix[10][10]) {
 int i,j;
 char temp[10][10];
 for (i = 0; i< 10; i++) {
@@ -20,4 +20,25 @@ for ( i = 0; i < 10 ; i++)   {
     }
     
   }
+}
+*/
+
+void swap(char *ptr1,char *ptr2){
+  char temp;
+  temp = *ptr1;
+  *ptr1 = *ptr2;
+  *ptr2 = temp;
+}
+void rotate(char matrix[10][10]){
+  int i = 0 , j = 0,k = 0;
+  int len = 10,z = 10;
+  
+  for(i = 0, z = 9;i<len/2;i++,z--){
+    for(j = i,k = z ;j<len-i-1;j++,k--){
+      swap(&matrix[i][j],&matrix[j][z]);
+      swap(&matrix[i][j],&matrix[k][i]);
+      swap(&matrix[k][i],&matrix[z][k]);
+    }
+  }
+  
 }
